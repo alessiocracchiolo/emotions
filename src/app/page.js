eaa8191b00ab1e58bff537b3f99783683f13e74c
Alessio <69542291+alessiocracchiolo@users.noreply.github.com>
2024-10-23 16:32:16 +0000

    start

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <h1>ciao</h1>
+    </div>
+  );
+}
